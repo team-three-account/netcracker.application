f791977225a055648b4e05c9d548633228432080
anntsit <anyatsit98@ukr.net>
2018-04-15 15:19:28 +0300

    add dependency for autogenerating getters and setters

--- src/main/java/com/gmail/netcracker/application/domain/Person.java
+++ src/main/java/com/gmail/netcracker/application/domain/Person.java
@@ -1,7 +1,10 @@
 package com.gmail.netcracker.application.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 
 @Entity
+@Getter // annotation that auto generate getters for all fields of class
 public class Person {
 }
