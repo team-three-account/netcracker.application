c53f86227027e0ae4bebe7ed35d9bfaef3f0668b
Artem <derevets.artem@gmail.com>
2018-05-17 17:25:58 +0300

    heroku

--- src/main/java/com/gmail/netcracker/application/controller/SearchController.java
+++ src/main/java/com/gmail/netcracker/application/controller/SearchController.java
@@ -2,6 +2,7 @@ package com.gmail.netcracker.application.controller;
 
 import com.gmail.netcracker.application.dto.model.User;
 import com.gmail.netcracker.application.service.interfaces.SearchService;
+import com.gmail.netcracker.application.service.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,13 +17,16 @@ public class SearchController {
     @Autowired
     private SearchService searchService;
 
+    @Autowired
+    private UserService userService;
+
     
     @RequestMapping(value = "/eventList/search", method = RequestMethod.POST)
     public String getSearch(Model model, String search) {
         if (search==null||search.isEmpty()) return "redirect:/account/eventList";
-        model.addAttribute("auth_user", authUser);
-        model.addAttribute("resultSearchPublic", searchService.searchPublicEvents(search, authUser));
-        model.addAttribute("resultSearchUserEvents", searchService.searchUserEvents(search, authUser));
+        model.addAttribute("auth_user", userService.getAuthenticatedUser());
+        model.addAttribute("resultSearchPublic", searchService.searchPublicEvents(search, userService.getAuthenticatedUser()));
+        model.addAttribute("resultSearchUserEvents", searchService.searchUserEvents(search, userService.getAuthenticatedUser()));
         return "event/resultSearch";
     }
 }
