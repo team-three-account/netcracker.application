2714127f28108a1a531d6bd2f61d4850294f3281
VladimirShuliak <VladimirShuliak20@gmail.com>
2018-05-20 01:23:17 +0300

    Add button for login end registration page. Add validation for item link and fix validation for item date.

--- src/main/java/com/gmail/netcracker/application/validation/ItemValidator.java
+++ src/main/java/com/gmail/netcracker/application/validation/ItemValidator.java
@@ -2,6 +2,7 @@ package com.gmail.netcracker.application.validation;
 
 import com.gmail.netcracker.application.dto.model.Item;
 import com.gmail.netcracker.application.utilites.Utilities;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.PropertySource;
@@ -14,35 +15,50 @@ import java.sql.Timestamp;
 @PropertySource(value = "classpath:message_en.properties")
 public class ItemValidator implements Validator {
 
-    @Autowired
-    MessageSource messageSource;
+  @Autowired
+  MessageSource messageSource;
 
-    @Override
-    public boolean supports(Class<?> aClass) {
-        return Item.class.equals(aClass);
-    }
+  @Override
+  public boolean supports(Class<?> aClass) {
+    return Item.class.equals(aClass);
+  }
 
-    @Override
-    public void validate(Object object, Errors errors) {
-        Item item = (Item) object;
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required.field");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "required.field");
-        if (expectedDate(item)) {
-            errors.rejectValue("dueDate", "required.currentDate");
-        }
+  @Override
+  public void validate(Object object, Errors errors) {
+    Item item = (Item) object;
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required.field");
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "required.field");
+    if (validationLink(item)) {
+      errors.rejectValue("link", "required.link");
     }
+    if (expectedDate(item)) {
+      errors.rejectValue("dueDate", "required.currentDate");
+    }
+  }
 
-    private boolean expectedDate(Item item) {
-        boolean status = false;
-        Timestamp currentDate = Utilities.getCurrentTimeStamp();
-        Timestamp setDate = Utilities.parseStringToTimestamp(item.getDueDate());
-        if (setDate != null) {
-            if (setDate.before(currentDate)) {
-                status = true;
-            }
-        }
-        return status;
+  private boolean expectedDate(Item item) {
+    boolean status = false;
+    Timestamp currentDate = Utilities.getCurrentTimeStamp();
+    Timestamp setDate = Utilities.parseStringToTimestampWithoutHours(item.getDueDate());
+    if (setDate != null) {
+      if (setDate.before(currentDate)) {
+        status = true;
+      }
     }
+    return status;
+  }
+
+  public boolean validationLink(Item item) {
+    boolean status = true;
+    String[] schemes = {"http", "https"};
+    if (!item.getLink().isEmpty()) {
+      UrlValidator urlValidator = new UrlValidator(schemes);
+      if (urlValidator.isValid(item.getLink())) {
+        status = false;
+      }
+    }
+    return status;
+  }
 }
 
 
