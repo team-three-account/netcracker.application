afc1c8b283f6f300a2aa3f081e8e1ef690b7b7db
Artem <derevets.artem@gmail.com>
2018-05-29 17:19:04 +0300

    refactor

--- src/main/java/com/gmail/netcracker/application/config/RootConfig.java
+++ src/main/java/com/gmail/netcracker/application/config/RootConfig.java
@@ -149,7 +149,7 @@ public class RootConfig  {
             eventMessage.setSenderId(getLong(resultSet, "sender_id"));
             eventMessage.setFrom(getString(resultSet, "sender_name"));
             eventMessage.setSenderPhoto(getString(resultSet, "sender_photo"));
-            notification.setCreatorEvent(getBoolean(resultSet, "creator_event"));
+            notification.setCreatorEvent(getBoolean(resultSet, "is_with_creator"));
             notification.setChatId(getLong(resultSet, "chat_id"));
             notification.setEvent(event);
             notification.setUser(user);
